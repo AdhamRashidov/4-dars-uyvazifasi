9d2930a1b1c94da89798cd82fa220ed2e69641df
adham <adhamrashidov0312@gmail.com>
2025-04-12 11:07:31 +0500

    homework 4

---
+++ index.js
@@ -0,0 +1,62 @@
+// //vazifa 1
+// let vaz1son1 = parseFloat(prompt("Birinchi sonni kiriting."));
+// let vaz1son2 = parseFloat(prompt("Ikkiinchi sonni kiriting."));
+// if (vaz1son1 < vaz1son2) {
+//     alert(`${vaz1son2} katta ${vaz1son1} dan.`);
+// } else if (vaz1son1 > vaz1son2) {
+//     alert(`${vaz1son1} katta ${vaz1son2} dan.`);
+// } else {
+//     alert("Ikkala son teng.")
+// }
+
+// //vazifa 2
+// let vaz2Son = prompt("Biror son kiriting.");
+// if (vaz2Son < 0) {
+//     alert("Bu son manfiy.");
+// } else if (vaz2Son > 0) {
+//     alert("Bu son musbat");
+// } else {
+//     alert ("Bu son '0' ga teng")
+// }
+
+// //vazifa 3
+// let vaz3son1 = Number(prompt("Birinchi sonni kiriting."));
+// let vaz3son2 = Number(prompt("Ikkinchi sonni kiriting."));
+// let vaz3amal = prompt("amalni tanlang: ( +, -, *, / )");
+// let vaz3res = 0;
+// switch (vaz3amal) {
+//     case '+':
+//         alert(`Ikki sonning yig'indisi ${vaz3son1 + vaz3son2}`);
+//         break;
+//     case '-':
+//         alert(`Ikki sonning ayirmasi ${vaz3son1 - vaz3son2}`);
+//         break;
+//     case '*':
+//         alert(`Ikki sonning ko'paytmasi ${vaz3son1 * vaz3son2}`);
+//         break;
+//     case '/':
+//         alert(`Ikki sonning bo'linmasi ${vaz3son1 / vaz3son2}`);
+//         break;
+
+//     default:
+//         alert("Bunday amal mavjud emas.");
+//         break;
+// }
+
+
+// // vazifa 4
+// let factorial = Number(prompt("Sonni kiriting."));
+// let element = 1
+// for (let i = 1; i <= factorial; i++) {
+//     element = element * i;
+// }
+// alert(`${factorial} Factorial = ${element} ga`);
+
+
+// vazifa 5
+let fibonacci = Number(prompt("Nechta fibonacci istaysiz"));
+let f1 = 0, f2 = 1;
+
+for(let i = 0; i < fibonacci; i++) {
+    
+}
